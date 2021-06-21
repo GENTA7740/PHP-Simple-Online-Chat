d0c1a943bd6d9f2981e3d2ee884ea6c16723e264
GENTA7740 <68140834+GENTA7740@users.noreply.github.com>
2021-06-21 23:23:15 +0700

    Add files via upload

---
+++ Chat.php
@@ -0,0 +1,8 @@
+<?php
+$username= $_POST['Username'];
+$message=  $_POST['Message'];
+$timestamp = date('h:i:s');
+$feed = 'Chat.txt';
+$fb = fopen($feed, 'a+');
+fwrite($fb, '['.$timestamp.'] ['.$username.'] : '.$message."\n\n");
+fclose($fb);
